1b445ed70125190ce6af67fa7dc5a6e3bcd2aa58
George Lugo <stg77713@gmail.com>
2020-03-11 02:02:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var recipies = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key] : value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  delete updateObjectWithKeyAndValue;
+  
+  return object
+}
